1d5aac4ef24246242bd7ce4790def5515fc4f75e
Anurag Patsariya <anurag.patsariya@outlook.com>
2016-05-08 10:10:13 -0700

    New changes

--- server.js
+++ server.js
@@ -4,16 +4,67 @@ var mongodb = require("mongodb");
 var mongoose = require("mongoose");
 var app = express();
 var MongoClient = mongodb.MongoClient;
+var session = require("express-session");
 app.use(express.static("."));
 app.use(express.static(__dirname + '/js'));
 app.use(express.static(__dirname + '/images'));
 app.use(bodyParser.json());
+app.use(session({ secret: "sh", cookie: { maxAge: 5 * 60 * 1000 } }));
+var sess;
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index2.html");
 });
 
+app.post("/logout", function (req, res) {
+     req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+    //res.send("username set to NULL");
+    
+});
+
+app.post("/login", function (req, res) {
+    var sess = req.session;
+    var username = req.body.username;
+    var pwd = req.body.pwd;
+    //console.log(username, pwd);
+    MongoClient.connect("mongodb://localhost:27017/user_data", function (err, db) {
+        if (err) {
+            console.log("Unable to connect for login.");
+        } else {
+            console.log("Connected for login verification.");
+            collection = db.collection("user_data");
+            collection.find({ "username": username, "password": pwd }).toArray(function (err, result) {
+                if (err) {
+                    console.log("Db error");
+                    res.send(err);
+                } else {
+                    console.log(result);
+                    //console.log(result.body.username, result.body.passwprd);
+                    if (result[0] != null) {
+                        console.log("Login Successful.");
+                        sess.user = result[0];
+                        console.log("Welcome "+result[0].username+"!!");
+                        res.send(result[0]);
+                    } else {
+                        console.log("Incorrect username or password.");
+                        res.send("failure");
+                    }
+
+                    //res.send(result);
+                }
+            });
+        }
+    });
+});
+
 app.get("/getEvents", function (req, res) {
     console.log("Ready to serve Data.");
+    console.log(sess);
     var collection;
     MongoClient.connect("mongodb://localhost:27017/event_data", function (err, db) {
         if (err) {
@@ -25,7 +76,7 @@ app.get("/getEvents", function (req, res) {
                 if (err) {
                     res.send(err);
                 } else {
-                    console.log(result);
+                    //console.log(result);
                     res.send(result);
                 }
             });
@@ -52,9 +103,11 @@ app.post("/registerUser", function (req, res) {
     //res.send("Data Recieved");
 });
 
-app.post("/addEvent", function (req, res) {
+app.post("/createEvent", function (req, res) {
     var doc = req.body;
-    console.log(doc);
+    console.log(sess.user);
+    //doc.addedBy = sess.user.username;
+    //console.log(doc);
     var collection;
     MongoClient.connect("mongodb://localhost:27017/event_data", function (err, db) {
         if (err) {
